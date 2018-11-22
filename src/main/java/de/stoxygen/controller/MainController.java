2826aee3e518a1513b00d699dcca1182b0405e19
hack3d <hack3d.dev@googlemail.com>
2018-11-22 21:46:30 +0100

    Add some comment.

--- src/main/java/de/stoxygen/controller/MainController.java
+++ src/main/java/de/stoxygen/controller/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 
+ */
 @Controller
 public class MainController {
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
